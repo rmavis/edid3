9e7054573d19ee35fe7b2e96f329727735284c65
Richard Mavis <richard@airtype.com>
2019-04-10 17:24:57 -0700

    Revise the way the v24 frame IDs are mapped to their titles/descriptions.
    And change how they're printed.

--- v24.go
+++ v24.go
@@ -42,206 +42,116 @@ func v24ReadFrames(reader *bufio.Reader) []ID3v2Frame {
 }
 
 func v24PrintFrames(frames []ID3v2Frame) {
+	pull := func (part [2]string) (string, string) {
+		return part[0], part[1]
+	}
+
+	keys := v24MakeFrameMap(pull)
+
 	for _, frame := range frames {
 		if frame.Header.Id[0:1] == "T" {
-			v24PrintTextFrame(frame)
+			fmt.Printf("%v: %v\n", keys[frame.Header.Id], parseString(frame.Body))
 		} else {
 			fmt.Printf("Frame is not text frame (%v)\n", frame.Header.Id)
 		}
 	}
 }
 
-func v24PrintTextFrame(frame ID3v2Frame) {
-	var key string
-	switch frame.Header.Id {
-	case "TALB":  //  Album/Movie/Show title
-		key = "Album"
-	case "TBPM":  //  BPM (beats per minute)
-		key = "BPM"
-	case "TCOM":  //  Composer
-		key = "Composer"
-	case "TCON":  //  Content type
-		key = "Content type"
-	case "TCOP":  //  Copyright message
-		key = "Copyright message"
-	case "TDEN":  //  Encoding time
-		key = "Encoding time"
-	case "TDLY":  //  Playlist delay
-		key = "Playlist delay"
-	case "TDOR":  //  Original release time
-		key = "Original release time"
-	case "TDRC":  //  Recording time
-		key = "Recording time"
-	case "TDRL":  //  Release time
-		key = "Release time"
-	case "TDTG":  //  Tagging time
-		key = "Tagging time"
-	case "TENC":  //  Encoded by
-		key = "Encoded by"
-	case "TEXT":  //  Lyricist/Text writer
-		key = "Lyricist/Text writer"
-	case "TFLT":  //  File type
-		key = "File type"
-	case "TIPL":  //  Involved people list
-		key = "Involved people list"
-	case "TIT1":  //  Content group description
-		key = "Content group description"
-	case "TIT2":  //  Title/songname/content description
-		key = "Title/songname/content description"
-	case "TIT3":  //  Subtitle/Description refinement
-		key = "Subtitle/Description refinement"
-	case "TKEY":  //  Initial key
-		key = "Initial key"
-	case "TLAN":  //  Language(s)
-		key = "Language(s)"
-	case "TLEN":  //  Length
-		key = "Length"
-	case "TMCL":  //  Musician credits list
-		key = "Musician credits list"
-	case "TMED":  //  Media type
-		key = "Media type"
-	case "TMOO":  //  Mood
-		key = "Mood"
-	case "TOAL":  //  Original album/movie/show title
-		key = "Original album/movie/show title"
-	case "TOFN":  //  Original filename
-		key = "Original filename"
-	case "TOLY":  //  Original lyricist(s)/text writer(s)
-		key = "Original lyricist(s)/text writer(s)"
-	case "TOPE":  //  Original artist(s)/performer(s)
-		key = "Original artist(s)/performer(s)"
-	case "TOWN":  //  File owner/licensee
-		key = "File owner/licensee"
-	case "TPE1":  //  Lead performer(s)/Soloist(s)
-		key = "Lead performer(s)/Soloist(s)"
-	case "TPE2":  //  Band/orchestra/accompaniment
-		key = "Band/orchestra/accompaniment"
-	case "TPE3":  //  Conductor/performer refinement
-		key = "Conductor/performer refinement"
-	case "TPE4":  //  Interpreted, remixed, or otherwise modified by
-		key = "Interpreted, remixed, or otherwise modified by"
-	case "TPOS":  //  Part of a set
-		key = "Part of a set"
-	case "TPRO":  //  Produced notice
-		key = "Produced notice"
-	case "TPUB":  //  Publisher
-		key = "Publisher"
-	case "TRCK":  //  Track number/Position in set
-		key = "Track number/Position in set"
-	case "TRSN":  //  Internet radio station name
-		key = "Internet radio station name"
-	case "TRSO":  //  Internet radio station owner
-		key = "Internet radio station owner"
-	case "TSOA":  //  Album sort order
-		key = "Album sort order"
-	case "TSOP":  //  Performer sort order
-		key = "Performer sort order"
-	case "TSOT":  //  Title sort order
-		key = "Title sort order"
-	case "TSRC":  //  ISRC (international standard recording code)
-		key = "ISRC (international standard recording code)"
-	case "TSSE":  //  Software/Hardware and settings used for encoding
-		key = "Software/Hardware and settings used for encoding"
-	case "TSST":  //  Set subtitle
-		key = "Set subtitle"
-	case "TXXX":  //  User defined text information frame
-		key = "User defined text information frame"
-	default:
-		key = frame.Header.Id
+// Full reference: http://id3.org/id3v2.4.0-frames
+func v24MakeFrameMap(pull func([2]string) (string, string)) map[string]string {
+	parts := [...][2]string{
+		[2]string{"AENC", "Audio encryption"},
+		[2]string{"APIC", "Attached picture"},
+		[2]string{"ASPI", "Audio seek point index"},
+		[2]string{"COMM", "Comments"},
+		[2]string{"COMR", "Commercial frame"},
+		[2]string{"ENCR", "Encryption method registration"},
+		[2]string{"EQU2", "Equalisation (2)"},
+		[2]string{"ETCO", "Event timing codes"},
+		[2]string{"GEOB", "General encapsulated object"},
+		[2]string{"GRID", "Group identification registration"},
+		[2]string{"LINK", "Linked information"},
+		[2]string{"MCDI", "Music CD identifier"},
+		[2]string{"MLLT", "MPEG location lookup table"},
+		[2]string{"OWNE", "Ownership frame"},
+		[2]string{"PRIV", "Private frame"},
+		[2]string{"PCNT", "Play counter"},
+		[2]string{"POPM", "Popularimeter"},
+		[2]string{"POSS", "Position synchronisation frame"},
+		[2]string{"RBUF", "Recommended buffer size"},
+		[2]string{"RVA2", "Relative volume adjustment (2)"},
+		[2]string{"RVRB", "Reverb"},
+		[2]string{"SEEK", "Seek frame"},
+		[2]string{"SIGN", "Signature frame"},
+		[2]string{"SYLT", "Synchronised lyric/text"},
+		[2]string{"SYTC", "Synchronised tempo codes"},
+		[2]string{"TALB", "Album/Movie/Show title"},
+		[2]string{"TBPM", "BPM (beats per minute)"},
+		[2]string{"TCOM", "Composer"},
+		[2]string{"TCON", "Content type"},
+		[2]string{"TCOP", "Copyright message"},
+		[2]string{"TDEN", "Encoding time"},
+		[2]string{"TDLY", "Playlist delay"},
+		[2]string{"TDOR", "Original release time"},
+		[2]string{"TDRC", "Recording time"},
+		[2]string{"TDRL", "Release time"},
+		[2]string{"TDTG", "Tagging time"},
+		[2]string{"TENC", "Encoded by"},
+		[2]string{"TEXT", "Lyricist/Text writer"},
+		[2]string{"TFLT", "File type"},
+		[2]string{"TIPL", "Involved people list"},
+		[2]string{"TIT1", "Content group description"},
+		[2]string{"TIT2", "Title/songname/content description"},
+		[2]string{"TIT3", "Subtitle/Description refinement"},
+		[2]string{"TKEY", "Initial key"},
+		[2]string{"TLAN", "Language(s)"},
+		[2]string{"TLEN", "Length"},
+		[2]string{"TMCL", "Musician credits list"},
+		[2]string{"TMED", "Media type"},
+		[2]string{"TMOO", "Mood"},
+		[2]string{"TOAL", "Original album/movie/show title"},
+		[2]string{"TOFN", "Original filename"},
+		[2]string{"TOLY", "Original lyricist(s)/text writer(s)"},
+		[2]string{"TOPE", "Original artist(s)/performer(s)"},
+		[2]string{"TOWN", "File owner/licensee"},
+		[2]string{"TPE1", "Lead performer(s)/Soloist(s)"},
+		[2]string{"TPE2", "Band/orchestra/accompaniment"},
+		[2]string{"TPE3", "Conductor/performer refinement"},
+		[2]string{"TPE4", "Interpreted, remixed, or otherwise modified by"},
+		[2]string{"TPOS", "Part of a set"},
+		[2]string{"TPRO", "Produced notice"},
+		[2]string{"TPUB", "Publisher"},
+		[2]string{"TRCK", "Track number/Position in set"},
+		[2]string{"TRSN", "Internet radio station name"},
+		[2]string{"TRSO", "Internet radio station owner"},
+		[2]string{"TSOA", "Album sort order"},
+		[2]string{"TSOP", "Performer sort order"},
+		[2]string{"TSOT", "Title sort order"},
+		[2]string{"TSRC", "ISRC (international standard recording code)"},
+		[2]string{"TSSE", "Software/Hardware and settings used for encoding"},
+		[2]string{"TSST", "Set subtitle"},
+		[2]string{"TXXX", "User defined text information frame"},
+		[2]string{"UFID", "Unique file identifier"},
+		[2]string{"USER", "Terms of use"},
+		[2]string{"USLT", "Unsynchronised lyric/text transcription"},
+		[2]string{"WCOM", "Commercial information"},
+		[2]string{"WCOP", "Copyright/Legal information"},
+		[2]string{"WOAF", "Official audio file webpage"},
+		[2]string{"WOAR", "Official artist/performer webpage"},
+		[2]string{"WOAS", "Official audio source webpage"},
+		[2]string{"WORS", "Official Internet radio station homepage"},
+		[2]string{"WPAY", "Payment"},
+		[2]string{"WPUB", "Publishers official webpage"},
+		[2]string{"WXXX", "User defined URL link frame"},
 	}
-	fmt.Printf("%v: %v\n", key, parseString(frame.Body))
+
+	_map := make(map[string]string)
+	for _, part := range parts {
+		key, val := pull(part)
+		_map[key] = val
+	}
+	return _map
 }
-
-
-/*
-
-//  Audio encryption
-"AENC" = null,
-//  Attached picture
-"APIC" = null,
-//  Audio seek point index
-"ASPI" = null,
-
-//  Comments
-"COMM" = null,
-//  Commercial frame
-"COMR" = null,
-
-//  Encryption method registration
-"ENCR" = null,
-//  Equalisation (2)
-"EQU2" = null,
-//  Event timing codes
-"ETCO" = null,
-
-//  General encapsulated object
-"GEOB" = null,
-//  Group identification registration
-"GRID" = null,
-
-//  Linked information
-"LINK" = null,
-
-//  Music CD identifier
-"MCDI" = null,
-//  MPEG location lookup table
-"MLLT" = null,
-
-//  Ownership frame
-"OWNE" = null,
-
-//  Private frame
-"PRIV" = null,
-//  Play counter
-"PCNT" = null,
-//  Popularimeter
-"POPM" = null,
-//  Position synchronisation frame
-"POSS" = null,
-
-//  Recommended buffer size
-"RBUF" = null,
-//  Relative volume adjustment (2)
-"RVA2" = null,
-//  Reverb
-"RVRB" = null,
-
-//  Seek frame
-"SEEK" = null,
-//  Signature frame
-"SIGN" = null,
-//  Synchronised lyric/text
-"SYLT" = null,
-//  Synchronised tempo codes
-"SYTC" = null,
-
-//  Unique file identifier
-"UFID" = null,
-//  Terms of use
-"USER" = null,
-//  Unsynchronised lyric/text transcription
-"USLT" = null,
-
-//  Commercial information
-"WCOM" = null,
-//  Copyright/Legal information
-"WCOP" = null,
-//  Official audio file webpage
-"WOAF" = null,
-//  Official artist/performer webpage
-"WOAR" = null,
-//  Official audio source webpage
-"WOAS" = null,
-//  Official Internet radio station homepage
-"WORS" = null,
-//  Payment
-"WPAY" = null,
-//  Publishers official webpage
-"WPUB" = null,
-//  User defined URL link frame
-"WXXX" = null,
-
-*/
 
 
 /*
